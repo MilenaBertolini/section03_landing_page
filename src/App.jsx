19259ed59fe8f9f9a4cf208b4706a9dac1690980
MilenaBertolini <milenaarantesb@gmail.com>
2024-08-21 09:28:38 -0300

    adicionando section 3 da landing page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Secao03 from './Secao03/Secao03'
+import CircleImageAndTitle from './CircleImageAndTitle/CircleImageAndTitle'
+
+
+function App() {
+
+  // const subtitleBoxes = ["Autoestima", "Bem-estar", "Saúde Integral", "Relacionamentos Saudáveis", "Autoconhecimento", "Resiliência"]
+
+  // const imagesBoxes = ["../Images/avatar.png","./Images/natural-mode.png", "./Images/dumbbell.png", "./Images/every-user.png", "./Images/crown-three.png", "./Images/escalators.png"]
+
+  
+
+  return (
+    <>
+      <Secao03/>
+    </>
+  )
+}
+
+export default App
